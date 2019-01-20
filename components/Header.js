677b6bc3e8597b2c4546040c53220eb445438ce1
Gil Tayar <gil@tayar.org>
2019-01-20 10:06:12 +0200

    added dealy-new-todo support

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react'
 import TodoTextInput from './TodoTextInput'
 
 class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      delayShowingNewTodo: parseInt(new URL(window.location.href).searchParams.get('delay-new-todo'), 10) || 0
+    }
+
+    if (this.state.delayShowingNewTodo > 0) {
+      setTimeout(this.handleEndOfDelayShowingNewTodo.bind(this), this.state.delayShowingNewTodo)
+    }
+  }
+
+  handleEndOfDelayShowingNewTodo() {
+    this.setState({delayShowingNewTodo: 0})
+  }
+
   handleSave(text) {
     if (text.length !== 0) {
       this.props.addTodo(text)
@@ -9,13 +24,18 @@ class Header extends Component {
   }
 
   render() {
+
     return (
       <header className="header">
           <h1>todos</h1>
           {/* <h1 style={{color:'green'}}>todos</h1> */}
-          <TodoTextInput newTodo
-                         onSave={this.handleSave.bind(this)}
-                         placeholder="What needs to be done?" />
+          {
+            this.state.delayShowingNewTodo === 0
+              ? <TodoTextInput newTodo
+                               onSave={this.handleSave.bind(this)}
+                               placeholder="What needs to be done?" />
+              : null
+          }
       </header>
     )
   }
